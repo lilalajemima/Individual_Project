c228fe5fffc80eab135fca1057e9584fbedaa7e7
lilalajemima <l.runiga@alustudent.com>
2024-07-16 15:51:16 -0700

    set a project outline with comments

---
+++ grade-book-app_Lilala_Runiga_Jemima/main.py
@@ -0,0 +1,45 @@
+#1/usr/bin/python3
+
+#Create the Student Class include documentation
+class Student:
+    def __init__(self, email, names, courses_registered, GPA):
+        self.email = email
+        self.names = names
+        self.courses_registered = courses_registered
+        self.GPA = GPA
+
+
+#Create the Coursers class include documentattion 
+class Courses:
+    def __init__(name, trimester, credits):
+        self.name = name
+        self.trimester = trimester
+        self.credits = credits
+
+#Create the grade book class 
+
+
+#Define the add_student method
+
+#Define the add_course() method
+
+#Define the register_student_for_course() method
+
+#Define the calculate_GPA() method
+
+#Define the calculate_ranking() method
+
+#Define the search_by_grade() method
+
+#Define the generate_transcript() method create the main program
+
+#Create instances of the Student and Course classes
+
+#Create an example of the Grade Book class
+
+#Display a menu of actions for the user to choose from
+
+#Implement the chosen action using the Grade Book class methods
+
+#Display the results of the selected action
+
